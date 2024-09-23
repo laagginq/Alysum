bb9558c2a9d664046ed78050db5d23f8b5132ea1
xz <95953670+laagginq@users.noreply.github.com>
2024-09-23 15:58:18 -0700

    Create direct_loadstrings.lua

---
+++ direct_loadstrings.lua
@@ -0,0 +1,24 @@
+-- // Alysum Direct Loadstrings
+-- // Use this if the loader is not working
+
+--[[
+    You must add the script_key="PASTE_KEY"; part before the loadstring
+        Example: 
+            script_key="PASTE_KEY";
+            loadstring(game:HttpGet("BLAH BLAH"))()
+]]
+
+-- Main
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2cb68af157ca24ff8de44ddc6c830f93.lua"))()
+
+-- Streamable 
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b432b5193c83baf427c3ef25794a7cb.lua"))()
+
+-- Solara
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b7b556786ae40eaf682115fc59fecc45.lua"))()
+
+-- Mobile 
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/24d045871c488464c0c15c2f0b2435b5.lua"))()
+
+-- Anti Lock GUI
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/331516c9f626f525655e26f87ebc0153.lua"))()
