76ff23bcf9988a31f03a65a5ea7ceb346cf413e5
xz <95953670+laagginq@users.noreply.github.com>
2024-09-23 16:01:41 -0700

    Update direct_loadstrings.lua

--- direct_loadstrings.lua
+++ direct_loadstrings.lua
@@ -18,7 +18,24 @@ loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b432b5193c83b
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b7b556786ae40eaf682115fc59fecc45.lua"))()
 
 -- Mobile (FREE)
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/24d045871c488464c0c15c2f0b2435b5.lua"))()
+getgenv().AlysumMobile = {
+    enabled = true, -- // Global Switch
+    ui = true, -- // Enable UI
+    givetool = true, -- // Give yourself the tool to select a target
+    button_position = {104,4}, -- // Button Position
+    legitness = "Super Legit", -- // Super Legit (Closest Point), Semi Legit (Closest Part), Normal (Torso), Blatant (Head)
+    prediction = "1", -- // Prediction version (1,2,3,custom)
+    custom_pred_x = 0.133,
+    custom_pred_y = 0.133,
+    notif = true, -- // Notifications
+    highlight = true, -- // Highlight Target
+    line = false, -- // Line to target
+    checks = {true, true, false}, -- // Visible, Knocked, Friend
+    assist = {false, 0.058}, -- // Enabled, Strength
+    fov = {true,50} -- // Visible, Size
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/laagginq/Alysum/main/mobile"))()
 
 -- Anti Lock GUI (FREE)
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/331516c9f626f525655e26f87ebc0153.lua"))()
